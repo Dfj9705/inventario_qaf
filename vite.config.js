eaf569ee1a02715c994caa90303a34b406c727af
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2025-11-02 09:57:45 -0600

    Añade coverage

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,10 @@ export default defineConfig({
   ],
   test: {
     environment: 'node',
+    coverage: {
+      provider: 'v8',             // usa @vitest/coverage-v8
+      reporter: ['text', 'lcov'],
+      reportsDirectory: 'coverage'
+    }
   },
 })
